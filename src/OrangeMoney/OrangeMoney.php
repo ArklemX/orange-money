a079cb878aa99aea0ae6902c19b73a1ea9de6f59
Avaika Nametissa Jean Marcel <80071206+ArklemX@users.noreply.github.com>
2023-01-11 13:30:23 +0100

    Update OrangeMoney.php

--- src/OrangeMoney/OrangeMoney.php
+++ src/OrangeMoney/OrangeMoney.php
@@ -6,6 +6,7 @@ class OrangeMoney
     //To get access token
     protected static string $om_token_url;
     protected static string $auth_token;
+    
 
     //TO Init the Merchant Payment
     protected static string $om_mp_init_url;
